ef9d00ffc5c8b583e7128095157fb7ae3df01d4f
Rahul Bansal <rahul@hudle.in>
2026-01-12 21:42:09 +0530

    Added to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    allowedHosts: ['bb.rbansal.xyz'],
+  },
+  preview: {
+    allowedHosts: ['bb.rbansal.xyz'],
+  },
 })
